668b0be65778888825e010bf3f521f9f4255c852
semg101 <gansosaker@yahoo.fr>
2020-05-14 08:43:39 +0100

    My project

---
+++ BankAccount/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankAccount
+{
+    class Program
+    {
+        public class BankAccount
+        {
+            //The list of the transactions
+            private List<Transaction> allTransactions = new List<Transaction>();
+            //It is used to initialize the account number
+            private static  int accountNumberSeed = 1234567890;
+            // a 10-digit number that uniquely identifies the bank account.
+            public string Number { get; }
+            //a string that stores the name or names of the owners.
+            public string Owner { get; set; }
+            //The balance can be retrieved.  The initial balance must be positive.
+            //summing the values of all transactions
+            public decimal Balance 
+            {
+                get
+                {
+                    decimal balance = 0;
+                    foreach (var item in allTransactions)
+                    {
+                        balance += item.Amount;
+                    }
+
+                    return balance;
+                }
+                set { this.Balance = value; }
+            }
+
+            public BankAccount(string name, decimal initialBalance)
+            {
+                this.Number = accountNumberSeed.ToString();
+                //accountNumberSeed++;
+
+                this.Owner = name;
+                MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+            }
+
+            //The bank account accepts deposits.
+            public void MakeDeposit(decimal amount, DateTime date, string note)
+            {
+                if (amount <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+                }
+                var deposit = new Transaction(amount, date, note);
+                allTransactions.Add(deposit);
+            }
+
+            //The bank account accepts withdrawals.
+            //any withdrawal must not create a negative balance
+            public void MakeWithdrawal(decimal amount, DateTime date, string note)
+            {
+                if (amount <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+                }
+                if (Balance - amount < 0)
+                {
+                    throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+                }
+                var withdrawal = new Transaction(-amount, date, note);
+                allTransactions.Add(withdrawal);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Saker", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
---
+++ BankAccount/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    //The bank account class needs to accept deposits and withdrawals
+    //Let's implement deposits and withdrawals by creating a journal of every transaction for the account
+    //updating the balance on each transaction
+    //The history can be used to audit all transactions and manage daily balances
+    //By computing the balance from the history of all transactions when needed,
+    //any errors in a single transaction that are fixed will be correctly reflected in the balance on the next computation.
+    class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
